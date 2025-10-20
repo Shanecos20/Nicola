8d2f837d0ae1adcccf83b0c1886b0f99325d1c27
Shane <g00391599@Atu.ie>
2025-10-20 15:07:28 +0100

    Nicola Wedding

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './App.css'
+import branchImage from './images/WeddingBranch.png'
+import leftSideImage from './images/3EB7529F-.jpeg'
+import rightSideImage from './images/d192e723-.jpeg'
+
+function App() {
+  return (
+    <>
+    <div className="side-image left">
+      <img src={leftSideImage} alt="Left decorative" />
+    </div>
+    <div className="side-image right">
+      <img src={rightSideImage} alt="Right decorative" />
+    </div>
+
+    <div className="container">
+      <div className="watermark"></div>
+      
+      <div className="leaf-decoration">
+        <img src={branchImage} alt="Wedding Branch Decoration" />
+      </div>
+
+      <div className="save-the-date">
+        SAVE <span className="the">the</span><br />DATE
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="wedding-of">FOR THE WEDDING OF</div>
+
+      <div className="names">Nicola & Connor</div>
+      
+      <div className="horizontal-line"></div>
+
+      <div className="divider"></div>
+
+      <div className="venue">
+        Shearwater Hotel & Spa, Ballinasloe, Co. Galway
+      </div>
+
+      <div className="date">24 . 10 . 26</div>
+    </div>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/LeafDecoration.jsx
@@ -0,0 +1,3 @@
+// This component is no longer used; keeping file to avoid import errors
+// in case of stale builds. You can delete this file safely.
+
